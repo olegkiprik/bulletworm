olegkiprik/snatan

Initial project version

Constants.hpp
@@ -1,0 +1,86 @@
+////////////////////////////////////////////////////////////
+//
+// Snatan - Extreme Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include "MiscEnum.hpp"
+#include <cstdint>
+
+namespace CrazySnakes {
+
+constexpr std::size_t NrParticles = 200;
+
+// text
+const char* const GameTitle = "Snatan";
+const char* const ShaderTimeTitle = "time";
+
+// textures
+constexpr unsigned int TexSz = 128;
+constexpr unsigned int TexUnitWidth = 8;
+constexpr unsigned int ThemeCount = 4;
+
+// music
+constexpr unsigned int MenuMusicId = 0;
+constexpr unsigned int LevelStatsMusicId = 1;
+
+// some limits
+constexpr unsigned int LevelCountMin = 1;
+constexpr unsigned int LevelCountMax = 150;
+constexpr unsigned int DiffCountMin = 2;
+constexpr unsigned int DiffCountMax = 5;
+
+constexpr std::int64_t TimeLimitMaxAsMcs = (std::int64_t)60 * 60 * 1000 * 1000;
+constexpr std::int64_t TimeLimitMinAsMcs = (std::int64_t)15 * 1000 * 1000;
+constexpr std::int64_t SnakePeriodMinAsMcs = (std::int64_t)10 * 1000;
+constexpr std::int64_t SnakePeriodMaxAsMcs = (std::int64_t)30 * 1000 * 1000;
+
+constexpr unsigned int BonusCountToPowerupMax = 100;
+constexpr unsigned int FruitCountToBonusMax = 100;
+
+constexpr std::uint32_t WidthMin = 3;
+constexpr std::uint32_t WidthMax = 0x1000;
+constexpr std::uint32_t HeightMin = 3;
+constexpr std::uint32_t HeightMax = 0x1000;
+
+constexpr std::size_t TriggerMapSize = (std::size_t)1 * 0x100000 + 1;
+
+// window config
+constexpr unsigned int ScaleYieldNumerator = 39;
+constexpr unsigned int ScaleYieldDenominator = 40;
+
+constexpr unsigned int WindowModeRatioNumerator = 3;
+constexpr unsigned int WindowModeRatioDenominator = 4;
+
+// 2560 -> 1920
+// 1920 -> 1440
+// 1440 -> 1080
+// 1080 -> 810
+
+constexpr std::uint64_t StatusHillEncryptionModulus = 65537;
+
+}
+
+#endif // CONSTANTS_HPP

Endianness.hpp
@@ -1,0 +1,37 @@
+////////////////////////////////////////////////////////////
+//
+// Snatan - Extreme Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef ENDIANNESS_HPP
+#define ENDIANNESS_HPP
+#include <cstdint>
+
+namespace CrazySnakes {
+
+std::uint32_t n2hl(std::uint32_t network);
+std::uint32_t h2nl(std::uint32_t host);
+
+}
+
+#endif // ENDIANNESS_HPP

LanguageEnums.hpp
@@ -1,0 +1,64 @@
+////////////////////////////////////////////////////////////
+//
+// Snatan - Extreme Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LANGUAGE_ENUMS_HPP
+#define LANGUAGE_ENUMS_HPP
+
+namespace CrazySnakes {
+
+enum class TimeUnit {
+	Week,
+	Day,
+	Hour,
+	Minute,
+	Second,
+	Millisecond,
+	Microsecond,
+	Count
+};
+
+// 0 - absolute singular (only 1)
+// 1 - absolute dual (only 2)
+// 2 - absolute paucal (only 3 or 4)
+// 3 - relative singular (21, 31)
+// 4 - relative dual (22, 32)
+// 5 - relative paucal (23, 34)
+// 6 - plural (0, 5, 11, 25, 100, 111)
+enum class LinguisticNumericType {
+	Single,
+	Dual,
+	Paucal,
+	SeparatedSingle,
+	SeparatedDual,
+	SeparatedPaucal,
+	Plural
+};
+
+
+constexpr int TimeUnitCount = static_cast<int>(TimeUnit::Count);
+
+}
+
+#endif // !LANGUAGE_ENUMS_HPP

Levels.hpp
@@ -1,0 +1,111 @@
+////////////////////////////////////////////////////////////
+//
+// Snatan - Extreme Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVELS_HPP
+#define LEVELS_HPP
+#include "EatableItem.hpp"
+#include <SFML/System/Vector2.hpp>
+#include <vector>
+#include <cstdint>
+#include <array>
+
+namespace {
+    template<class T>
+    constexpr unsigned int fwkBitCeilLogLvl(T num) noexcept {
+        unsigned int nr = 0;
+        while (num) {
+            num >>= 1;
+            ++nr;
+        }
+        return nr;
+    }
+
+    template<class T, class U>
+    constexpr T fwkGetRealSizeLvl(U size) noexcept {
+        return 1 + (size ? (T(1) << fwkBitCeilLogLvl(size - 1)) : T(0));
+    }
+}
+
+namespace sf {
+class InputStream;
+}
+
+namespace CrazySnakes {
+
+enum class LevelCountMap;
+enum class EatableItem;
+
+class Levels {
+public:
+
+    [[nodiscard]] bool loadFromStream(unsigned int diffCount,
+                                      unsigned int levelCount, sf::InputStream& stream,
+                                      bool endiannessRequired);
+
+    unsigned int getDifficultyCount() const noexcept {
+        return m_diffCount;
+    }
+    unsigned int getLevelCount() const noexcept {
+        return m_levelCount;
+    }
+
+    const std::uint32_t* getLevelAttribPtr(unsigned int diffIndex, 
+                                           unsigned int levelIndex) const noexcept;
+    const std::uint32_t* getLevelPlotDataPtr(unsigned int diffIndex,
+                                             unsigned int levelIndex) const noexcept;
+    const std::uint32_t* getEffectDurationPtr(unsigned int diffIndex, 
+                                              unsigned int levelIndex) const noexcept;
+    const std::array<std::uintmax_t, fwkGetRealSizeLvl<std::size_t,int>(PowerupCount)>&
+        getPowerupProbs(unsigned int diffIndex, unsigned int levelIndex) const noexcept;
+    const sf::Vector2u& getMapSize(unsigned int diffIndex,
+                                   unsigned int levelIndex) const noexcept;
+
+    const std::uint32_t*
+        getLevelCountMap(LevelCountMap what, unsigned int diffIndex,
+                         unsigned int levelIndex) const noexcept;
+    const std::uint32_t*
+        getItemProbCountMap(EatableItem what, unsigned int diffIndex, 
+                            unsigned int levelIndex) const noexcept;
+
+private:
+
+    std::vector<std::uint32_t> m_levelAttributes;
+    std::vector<std::uint32_t> m_levelPlotData;
+    std::vector<std::uint32_t> m_effectDurations;
+    
+    std::vector<std::array<std::uintmax_t,
+        fwkGetRealSizeLvl<std::size_t, int>(PowerupCount)>> m_powerupProbs;
+
+    std::vector<sf::Vector2u> m_mapSizes;
+    std::vector<std::vector<std::uint32_t>> m_levelCountMaps;
+    std::vector<std::vector<std::uint32_t>> m_itemProbCountMaps;
+
+    unsigned int m_diffCount = 0;
+    unsigned int m_levelCount = 0;
+};
+
+}
+
+#endif // !LEVELS_HPP

MiscEnum.hpp
@@ -1,0 +1,53 @@
+////////////////////////////////////////////////////////////
+//
+// Snatan - Extreme Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef MISC_ENUM_HPP
+#define MISC_ENUM_HPP
+
+namespace CrazySnakes {
+	// Different randomizers to different dsts
+enum class RandomizerType {
+	Position,  // position
+	Powerup,   // kind
+	Behaviour, // random param
+
+	Count
+};
+
+constexpr int RandomTypeCount = static_cast<int>(RandomizerType::Count);
+
+// Challenge type to win
+enum class ChallengeType {
+	Fruits,
+	Bonuses,
+	Powerups,
+
+	Count
+};
+
+constexpr int ChallengeCount = static_cast<int>(ChallengeType::Count);
+}
+
+#endif // !MISC_ENUM_HPP

Orientation.hpp
@@ -1,0 +1,49 @@
+////////////////////////////////////////////////////////////
+//
+// Snatan - Extreme Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef ORIENTATION_HPP
+#define ORIENTATION_HPP
+
+namespace CrazySnakes {
+
+// Orthogonal two-dimensional orientation
+enum class Orientation {
+	Identity,                // No effect
+	FlipHorizontally,        // Flip on vertical axis
+	RotateClockwise,         // Rotate clockwise (right-down system)
+	InverseTranspose,        // Flip on alternative diagonal (./.)
+	Flip,                    // Rotate on 180 degrees
+	FlipVertically,          // Flip on horizontal axis
+	RotateCounterClockwise,  // Rotate counter-clockwise (right-down system)
+	MainTranspose,           // Flip on main diagonal (.\.)
+
+	Count
+};
+
+constexpr int OrientationCount = (int)Orientation::Count;
+
+}
+
+#endif // !ORIENTATION_HPP

olegkiprik/bulletworm

src/Constants.hpp
@@ -1,0 +1,86 @@
+////////////////////////////////////////////////////////////
+//
+// Bulletworm - Advanced Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include "MiscEnum.hpp"
+#include <cstdint>
+
+namespace Bulletworm {
+
+constexpr std::size_t NrParticles = 200;
+
+// text
+const char* const GameTitle = "Bulletworm";
+const char* const ShaderTimeTitle = "time";
+
+// textures
+constexpr unsigned int TexSz = 128;
+constexpr unsigned int TexUnitWidth = 8;
+constexpr unsigned int ThemeCount = 4;
+
+// music
+constexpr unsigned int MenuMusicId = 0;
+constexpr unsigned int LevelStatsMusicId = 1;
+
+// some limits
+constexpr unsigned int LevelCountMin = 1;
+constexpr unsigned int LevelCountMax = 150;
+constexpr unsigned int DiffCountMin = 2;
+constexpr unsigned int DiffCountMax = 5;
+
+constexpr std::int64_t TimeLimitMaxAsMcs = (std::int64_t)60 * 60 * 1000 * 1000;
+constexpr std::int64_t TimeLimitMinAsMcs = (std::int64_t)15 * 1000 * 1000;
+constexpr std::int64_t SnakePeriodMinAsMcs = (std::int64_t)10 * 1000;
+constexpr std::int64_t SnakePeriodMaxAsMcs = (std::int64_t)30 * 1000 * 1000;
+
+constexpr unsigned int BonusCountToPowerupMax = 100;
+constexpr unsigned int FruitCountToBonusMax = 100;
+
+constexpr std::uint32_t WidthMin = 3;
+constexpr std::uint32_t WidthMax = 0x1000;
+constexpr std::uint32_t HeightMin = 3;
+constexpr std::uint32_t HeightMax = 0x1000;
+
+constexpr std::size_t TriggerMapSize = (std::size_t)1 * 0x100000 + 1;
+
+// window config
+constexpr unsigned int ScaleYieldNumerator = 39;
+constexpr unsigned int ScaleYieldDenominator = 40;
+
+constexpr unsigned int WindowModeRatioNumerator = 3;
+constexpr unsigned int WindowModeRatioDenominator = 4;
+
+// 2560 -> 1920
+// 1920 -> 1440
+// 1440 -> 1080
+// 1080 -> 810
+
+constexpr std::uint64_t StatusHillEncryptionModulus = 65537;
+
+}
+
+#endif // CONSTANTS_HPP

src/Endianness.hpp
@@ -1,0 +1,37 @@
+////////////////////////////////////////////////////////////
+//
+// Bulletworm - Advanced Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef ENDIANNESS_HPP
+#define ENDIANNESS_HPP
+#include <cstdint>
+
+namespace Bulletworm {
+
+std::uint32_t n2hl(std::uint32_t network);
+std::uint32_t h2nl(std::uint32_t host);
+
+}
+
+#endif // ENDIANNESS_HPP

src/LanguageEnums.hpp
@@ -1,0 +1,64 @@
+////////////////////////////////////////////////////////////
+//
+// Bulletworm - Advanced Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LANGUAGE_ENUMS_HPP
+#define LANGUAGE_ENUMS_HPP
+
+namespace Bulletworm {
+
+enum class TimeUnit {
+	Week,
+	Day,
+	Hour,
+	Minute,
+	Second,
+	Millisecond,
+	Microsecond,
+	Count
+};
+
+// 0 - absolute singular (only 1)
+// 1 - absolute dual (only 2)
+// 2 - absolute paucal (only 3 or 4)
+// 3 - relative singular (21, 31)
+// 4 - relative dual (22, 32)
+// 5 - relative paucal (23, 34)
+// 6 - plural (0, 5, 11, 25, 100, 111)
+enum class LinguisticNumericType {
+	Single,
+	Dual,
+	Paucal,
+	SeparatedSingle,
+	SeparatedDual,
+	SeparatedPaucal,
+	Plural
+};
+
+
+constexpr int TimeUnitCount = static_cast<int>(TimeUnit::Count);
+
+}
+
+#endif // !LANGUAGE_ENUMS_HPP

src/Levels.hpp
@@ -1,0 +1,111 @@
+////////////////////////////////////////////////////////////
+//
+// Bulletworm - Advanced Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVELS_HPP
+#define LEVELS_HPP
+#include "EatableItem.hpp"
+#include <SFML/System/Vector2.hpp>
+#include <vector>
+#include <cstdint>
+#include <array>
+
+namespace {
+    template<class T>
+    constexpr unsigned int fwkBitCeilLogLvl(T num) noexcept {
+        unsigned int nr = 0;
+        while (num) {
+            num >>= 1;
+            ++nr;
+        }
+        return nr;
+    }
+
+    template<class T, class U>
+    constexpr T fwkGetRealSizeLvl(U size) noexcept {
+        return 1 + (size ? (T(1) << fwkBitCeilLogLvl(size - 1)) : T(0));
+    }
+}
+
+namespace sf {
+class InputStream;
+}
+
+namespace Bulletworm {
+
+enum class LevelCountMap;
+enum class EatableItem;
+
+class Levels {
+public:
+
+    [[nodiscard]] bool loadFromStream(unsigned int diffCount,
+                                      unsigned int levelCount, sf::InputStream& stream,
+                                      bool endiannessRequired);
+
+    unsigned int getDifficultyCount() const noexcept {
+        return m_diffCount;
+    }
+    unsigned int getLevelCount() const noexcept {
+        return m_levelCount;
+    }
+
+    const std::uint32_t* getLevelAttribPtr(unsigned int diffIndex, 
+                                           unsigned int levelIndex) const noexcept;
+    const std::uint32_t* getLevelPlotDataPtr(unsigned int diffIndex,
+                                             unsigned int levelIndex) const noexcept;
+    const std::uint32_t* getEffectDurationPtr(unsigned int diffIndex, 
+                                              unsigned int levelIndex) const noexcept;
+    const std::array<std::uintmax_t, fwkGetRealSizeLvl<std::size_t,int>(PowerupCount)>&
+        getPowerupProbs(unsigned int diffIndex, unsigned int levelIndex) const noexcept;
+    const sf::Vector2u& getMapSize(unsigned int diffIndex,
+                                   unsigned int levelIndex) const noexcept;
+
+    const std::uint32_t*
+        getLevelCountMap(LevelCountMap what, unsigned int diffIndex,
+                         unsigned int levelIndex) const noexcept;
+    const std::uint32_t*
+        getItemProbCountMap(EatableItem what, unsigned int diffIndex, 
+                            unsigned int levelIndex) const noexcept;
+
+private:
+
+    std::vector<std::uint32_t> m_levelAttributes;
+    std::vector<std::uint32_t> m_levelPlotData;
+    std::vector<std::uint32_t> m_effectDurations;
+    
+    std::vector<std::array<std::uintmax_t,
+        fwkGetRealSizeLvl<std::size_t, int>(PowerupCount)>> m_powerupProbs;
+
+    std::vector<sf::Vector2u> m_mapSizes;
+    std::vector<std::vector<std::uint32_t>> m_levelCountMaps;
+    std::vector<std::vector<std::uint32_t>> m_itemProbCountMaps;
+
+    unsigned int m_diffCount = 0;
+    unsigned int m_levelCount = 0;
+};
+
+}
+
+#endif // !LEVELS_HPP

src/MiscEnum.hpp
@@ -1,0 +1,53 @@
+////////////////////////////////////////////////////////////
+//
+// Bulletworm - Advanced Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef MISC_ENUM_HPP
+#define MISC_ENUM_HPP
+
+namespace Bulletworm {
+	// Different randomizers to different dsts
+enum class RandomizerType {
+	Position,  // position
+	Powerup,   // kind
+	Behavior, // random param
+
+	Count
+};
+
+constexpr int RandomTypeCount = static_cast<int>(RandomizerType::Count);
+
+// Challenge type to win
+enum class ChallengeType {
+	Fruits,
+	Bonuses,
+	Powerups,
+
+	Count
+};
+
+constexpr int ChallengeCount = static_cast<int>(ChallengeType::Count);
+}
+
+#endif // !MISC_ENUM_HPP

src/Orientation.hpp
@@ -1,0 +1,49 @@
+////////////////////////////////////////////////////////////
+//
+// Bulletworm - Advanced Snake Game
+// Copyright (c) 2024 Oleh Kiprik (oleg.kiprik@proton.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef ORIENTATION_HPP
+#define ORIENTATION_HPP
+
+namespace Bulletworm {
+
+// Orthogonal two-dimensional orientation
+enum class Orientation {
+	Identity,                // No effect
+	FlipHorizontally,        // Flip on vertical axis
+	RotateClockwise,         // Rotate clockwise (right-down system)
+	InverseTranspose,        // Flip on alternative diagonal (./.)
+	Flip,                    // Rotate on 180 degrees
+	FlipVertically,          // Flip on horizontal axis
+	RotateCounterClockwise,  // Rotate counter-clockwise (right-down system)
+	MainTranspose,           // Flip on main diagonal (.\.)
+
+	Count
+};
+
+constexpr int OrientationCount = (int)Orientation::Count;
+
+}
+
+#endif // !ORIENTATION_HPP