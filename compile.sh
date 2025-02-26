c0e66b4ff0dddcc25afaf67ac4be08711b150213
olegkiprik <oleg.kiprik@proton.me>
2025-02-26 15:03:46 +0100

    Remove hardcoded file paths

---
+++ compile.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# export SFML_SRC_PATH first
+g++ \
+-pipe \
+-std=c++17 \
+-s \
+-DNDEBUG \
+-O2 \
+-march=native \
+-fno-builtin \
+-fwhole-program \
+-flto \
+-ffast-math \
+src/*.c* \
+src/engine/*.c* \
+$SFML_SRC_PATH/SFML-2.6.1/src/SFML/Audio/*.c* \
+$SFML_SRC_PATH/SFML-2.6.1/src/SFML/Graphics/*.c* \
+$SFML_SRC_PATH/SFML-2.6.1/src/SFML/Window/*.c* \
+$SFML_SRC_PATH/SFML-2.6.1/src/SFML/System/*.c* \
+$SFML_SRC_PATH/SFML-2.6.1/src/SFML/System/Unix/*.c* \
+$SFML_SRC_PATH/SFML-2.6.1/src/SFML/Window/Unix/*.c* \
+-I $SFML_SRC_PATH/SFML-2.6.1/include/ \
+-I $SFML_SRC_PATH/SFML-2.6.1/src/ \
+-I $SFML_SRC_PATH/SFML-2.6.1/extlibs/headers/AL \
+-I $SFML_SRC_PATH/SFML-2.6.1/extlibs/headers/glad/include/ \
+-I $SFML_SRC_PATH/SFML-2.6.1/extlibs/headers/minimp3/ \
+-I $SFML_SRC_PATH/SFML-2.6.1/extlibs/headers/freetype2/ \
+-I $SFML_SRC_PATH/SFML-2.6.1/extlibs/headers/stb_image/ \
+-I $SFML_SRC_PATH/SFML-2.6.1/extlibs/headers/vulkan/ \
+-o bulletworm \
+-l openal \
+-l GL \
+-l freetype \
+-l X11 \
+-l pthread \
+-l m \
+-l vulkan \
+-l udev \
+-l vorbis \
+-l ogg \
+-l FLAC \
+-l Xrandr \
+-l Xcursor \
+-l vorbisfile \
+-l vorbisenc
